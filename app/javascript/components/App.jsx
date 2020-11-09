860319188a4cf8f1fde3777edf45d44771b017d4
Henrique <hp_bonfim@hotmail.com>
2020-11-09 05:26:04 -0300

    add the first components

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Airlines from './Airlines'
+import Airline from './Airline'
 
 const App = () => {
     return (
-        <div>Hello World</div>
-      )
+        <Switch>
+            <Route exact path="/" component={Airlines} />
+            <Route exact path="/:slug" component={Airline} />
+        </Switch>
+    )
 }
 
 export default App
